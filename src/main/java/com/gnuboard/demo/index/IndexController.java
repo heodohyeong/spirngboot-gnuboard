e9a6005791772fe1b36b2dc5436d024819ddeb5a
hdh <jackt5000@yooncoms.com>
2023-09-18 17:02:26 +0900

    thymeleaf - index

--- src/main/java/com/gnuboard/demo/index/IndexController.java
+++ src/main/java/com/gnuboard/demo/index/IndexController.java
@@ -1,14 +1,17 @@
 package com.gnuboard.demo.index;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model, HttpServletRequest request){
+        model.addAttribute("request", request);
         return "index";
     }
 }

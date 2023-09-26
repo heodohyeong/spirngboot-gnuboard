8d5db3480b8ccca7effc5bc69d0d86441c708ca4
hdh <jackt5000@yooncoms.com>
2023-09-26 10:44:32 +0900

    jpa paging index 1부터 시작

--- src/main/java/com/gnuboard/demo/DemoApplication.java
+++ src/main/java/com/gnuboard/demo/DemoApplication.java
@@ -3,7 +3,12 @@ package com.gnuboard.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DemoApplication {
 
@@ -11,4 +16,12 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+
+    @Bean
+    public PageableHandlerMethodArgumentResolverCustomizer customize() {
+        return p -> {
+            p.setOneIndexedParameters(true);	// 1부터 시작
+            //p.setMaxPageSize(10);				// size=10
+        };
+    }
 }
